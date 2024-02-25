1a4a15855d19ecf2a101acb4f0cdd3f3a2b91325
Vass Paul <vass_paul95@yahoo.com>
2024-02-25 13:37:47 +0200

    added hobbies for person instance, added instance to hashMap, printed hobby + countries for person instance

--- src/main/java/week5/TaskWeek5.java
+++ src/main/java/week5/TaskWeek5.java
@@ -1,8 +1,11 @@
 package week5;
 
 import java.util.Comparator;
+import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
 
 public class TaskWeek5 {
     public static void main(String[] args) {
@@ -24,5 +27,21 @@ public class TaskWeek5 {
 
         // iterating through the treeSet and printing each person's name + age
         persons.forEach(System.out::println);
+
+        ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        System.out.println();
+        person1.addHobbies(List.of(
+                new Hobby("cycling", 3,
+                        List.of(new Address(Country.ROMANIA, "home"), new Address(Country.GERMANY, "gym"))),
+                new Hobby("swimming", 2,
+                        List.of(new Address(Country.FRANCE, "pool"), new Address(Country.ENGLAND, "beach")))));
+
+        Map<Person, List<Hobby>> personHobbies = new HashMap<>();
+        personHobbies.put(person1, person1.getHobbies());
+
+        for (Person p : personHobbies.keySet()) {
+            System.out.println(p + ", hobbies: ");
+            personHobbies.get(p).forEach(System.out::println);
+        }
     }
 }
