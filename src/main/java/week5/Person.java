586073380cdec0c9f7322e1b0dc244882baa8177
Vass Paul <vass_paul95@yahoo.com>
2024-02-25 12:25:15 +0200

    added Person class

---
+++ src/main/java/week5/Person.java
@@ -0,0 +1,25 @@
+package week5;
+
+public class Person {
+
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + age + " years old";
+    }
+}
