b099b239d18f2d585547ef10f2e6fb3606c52a64
Vass Paul <vass_paul95@yahoo.com>
2024-03-19 18:41:20 +0200

    added BiathlonAthlete class

---
+++ src/main/java/week7/BiathlonAthlete.java
@@ -0,0 +1,72 @@
+package week7;
+
+public class BiathlonAthlete {
+
+    private final int athleteNumber;
+    private final String athleteName;
+    private final CountryCode countryCode;
+    private final int resultMinutes;
+    private final int resultSeconds;
+    private final String firstShooting;
+    private final String secondShooting;
+    private final String thirdShooting;
+
+    private int finalResultMinutes;
+    private int finalResultSeconds;
+    private int addedSeconds;
+
+    public BiathlonAthlete(int athleteNumber, String athleteName, CountryCode countryCode, int resultMinutes, int resultSeconds, String firstShooting, String secondShooting, String thirdShooting) {
+        this.athleteNumber = athleteNumber;
+        this.athleteName = athleteName;
+        this.countryCode = countryCode;
+        this.resultMinutes = resultMinutes;
+        this.resultSeconds = resultSeconds;
+        this.firstShooting = firstShooting;
+        this.secondShooting = secondShooting;
+        this.thirdShooting = thirdShooting;
+
+        finalResultMinutes = resultMinutes;
+        finalResultSeconds = resultSeconds;
+
+        calculateFinalTime();
+    }
+
+    public int getFinalResultMinutes() {
+        return finalResultMinutes;
+    }
+
+    public int getFinalResultSeconds() {
+        return finalResultSeconds;
+    }
+
+    private int calculateAddedSeconds() {
+        final String s = new StringBuilder(firstShooting)
+                .append(secondShooting)
+                .append(thirdShooting)
+                .toString();
+
+        int misses = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (String.valueOf(s.charAt(i)).equalsIgnoreCase("o")) {
+                misses++;
+            }
+        }
+        return misses * 10;
+    }
+
+    private void calculateFinalTime() {
+        addedSeconds = calculateAddedSeconds();
+
+        int totalSeconds = addedSeconds + resultSeconds;
+        int addedMinutes = totalSeconds / 60;
+        int addedSeconds = totalSeconds % 60;
+
+        finalResultMinutes += addedMinutes;
+        finalResultSeconds = addedSeconds;
+    }
+
+    @Override
+    public String toString() {
+        return athleteName + " " + finalResultMinutes + ":" + finalResultSeconds + " (" + resultMinutes + ":" + resultSeconds + " + " + addedSeconds + ")";
+    }
+}
