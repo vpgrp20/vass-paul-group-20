b4e8c7434a345b9e09c362820087c05141359ee1
Vass Paul <vass_paul95@yahoo.com>
2024-03-19 20:20:59 +0200

    main update

--- src/main/java/week7/Main.java
+++ src/main/java/week7/Main.java
@@ -4,8 +4,6 @@ import java.nio.file.Path;
 
 public class Main {
     public static void main(String[] args) {
-//        BiathlonAthlete athlete = new BiathlonAthlete(11, "Umar Jorgson", CountryCode.CZ, 30, 27, "xxxox", "xxxxx", "xxoxo");
-//        System.out.println(athlete);
         Path biathlonResults = Path.of("src/main/resources/AthleteResults.txt");
         BiathlonResultCalculator biathlon = new BiathlonResultCalculator(biathlonResults);
         biathlon.calculateResults();
