b087bf4d3cac840593ca4d7dee15689288511db3
Vass Paul <vass_paul95@yahoo.com>
2024-04-04 15:03:29 +0300

    FileProcessingApplication class updates

--- src/main/java/week10/FileProcessingApplication.java
+++ src/main/java/week10/FileProcessingApplication.java
@@ -1,11 +1,39 @@
 package week10;
 
-public class FileProcessingApplication {
-    //todo
-    public void processFiles(String inputFile, int targetMonth, String outputFile) {
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Stream;
 
-//        Files.lines(filePath)
-//                .map(line -> line.split(" "))
-//                .forEach(lineArray -> new User(lineArray[0], lineArray[1], lineArray[2]));
+public class FileProcessingApplication {
+
+    public void processFiles(String inputFile, int targetMonth, String outputFile) {
+        Path input = Path.of(inputFile);
+        Path output = Path.of(outputFile);
+
+        try (Stream<String> lines = Files.lines(input);
+             BufferedWriter writer = Files.newBufferedWriter(output)) {
+
+            final List<User> users = lines.map(line -> line.split(","))
+                    .map(line -> new User(line[0], line[1], line[2].split("\\.")))
+                    .toList();
+
+            users.stream()
+                    .filter(user -> user.getDoB().getMonth().getValue() == targetMonth)
+                    .sorted(Comparator.comparing(User::getLastName).thenComparing(User::getFirstName))
+                    .forEach(user -> {
+                        try {
+                            writer.write(user.getFirstName() + ", " + user.getLastName() + "\n");
+                        } catch (IOException e) {
+                            throw new RuntimeException(e);
+                        }
+                    });
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
