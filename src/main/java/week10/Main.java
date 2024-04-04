0a31fd3c08c56eba7135b15d23371bfc9536f8d8
Vass Paul <vass_paul95@yahoo.com>
2024-04-04 15:04:34 +0300

    running the app in main

--- src/main/java/week10/Main.java
+++ src/main/java/week10/Main.java
@@ -2,6 +2,7 @@ package week10;
 
 public class Main {
     public static void main(String[] args) {
-
+        FileProcessingApplication app = new FileProcessingApplication();
+        app.processFiles("src/main/resources/ResourceFileWeek10", 3, "src/main/java/week10/OutputFile.txt");
     }
 }
